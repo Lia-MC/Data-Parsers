52e0c8cf29f4976b09d257af33cd4cf3b5584b22
Lia-MC <lia.mc73700@gmail.com>
2025-06-12 05:11:00 -0400

    restructured main and added functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-int main() {
+int loadfile(filename) {
     // load text file
-    ifstream f("samplevals.txt");
+    ifstream f(filename); // ifstream f("samplevals.txt");
 
     // check if file opened successfully
     if (!f.is_open()) {
@@ -12,18 +12,26 @@ int main() {
     string s;
 
     // read each line of the file, 
-    // store it in string s and print it to the standard output stream 
-    while (getline(f, s))
-        cout << s << endl;
+    // store it in string s
+    while (getline(f, s)) {
+    }
 
     // Close the file
     f.close();
-    return 0;
 
     // while char isnt null
     // read in first num, ignore second and third, read in fourth num
     // repeat for each row until char is null then exit loop
     // display the two columns (each time just add word1 word2 newline for each row u read in)
-    cout << "ran";
     return 0;
+}
+
+int main() {
+    // i need to make sure that i can access string s, maybe i malloc it here, then pass it into load file
+
+    while (loadfile() != 0) {
+        filename = ; // get user input
+    }
+
+    cout << "ran";
 }
